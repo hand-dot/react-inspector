edd026536723483ed46128cddfdf2cb649d8846c
Kyohei Fukuda <kyouhei.fukuda0729@gmail.com>
2022-09-01 14:47:39 +0900

    Change inspector

--- src/content-main-world.ts
+++ src/content-main-world.ts
@@ -1,82 +1,37 @@
-declare global {
-  interface Window {
-    __REACT_DEVTOOLS_GLOBAL_HOOK__: any;
-  }
-}
+import {
+  checkDevtoolsGlobalHook,
+  getVsCodeLink,
+  findFiberByHostInstance,
+} from "./utils";
+import Overlay from "./Overlay";
 
-interface DebugSource {
-  columnNumber: number;
-  fileName: string;
-  lineNumber: number;
-}
-
-const getVsCodeLink = (sourceCode: DebugSource) =>
-  `vscode://file${sourceCode.fileName}:${sourceCode.lineNumber}:${sourceCode.columnNumber}`;
-
-const checkDevtoolsGlobalHookRenderer = (): boolean =>
-  window.__REACT_DEVTOOLS_GLOBAL_HOOK__ &&
-  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers &&
-  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers.size > 0 &&
-  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers.get(1);
-
-const findFiberByHostInstance = (
-  target: HTMLElement
-): { _debugSource: DebugSource } | null => {
-  if (!checkDevtoolsGlobalHookRenderer()) return null;
-
-  const renderer = window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers.get(1);
-  if (!renderer) return null;
-
-  const fiber = renderer.findFiberByHostInstance(target) || null;
-
-  return fiber && fiber._debugSource ? fiber : null;
-};
+let overlay: Overlay | null = null;
 
 let inspecting = false;
 
 const startInspectorMode = () => {
   inspecting = true;
-  inspector.style.display = "block";
-  window.addEventListener("pointerover", handleElementPointerover);
-  window.addEventListener("click", handleInspectorClick);
+  if (!overlay) {
+    overlay = new Overlay();
+  }
+  window.addEventListener("pointerover", handleElementPointerOver, true);
+  window.addEventListener("click", handleInspectorClick, true);
 };
 
 const exitInspectorMode = () => {
   inspecting = false;
-  inspector.style.display = "none";
-  window.removeEventListener("pointerover", handleElementPointerover);
-  window.removeEventListener("click", handleInspectorClick);
+  if (overlay) {
+    overlay.remove();
+    overlay = null;
+  }
+  window.removeEventListener("pointerover", handleElementPointerOver, true);
+  window.removeEventListener("click", handleInspectorClick, true);
 };
 
-const handleElementPointerover = (e: PointerEvent) => {
+const handleElementPointerOver = (e: PointerEvent) => {
   const target = e.target as HTMLElement | null;
-
   if (!target) return;
-
-  const boundingClientRect = target.getBoundingClientRect();
-  const { top, left, width, height } = boundingClientRect;
-  inspector.style.top = `${top + window.pageYOffset}px`;
-  inspector.style.left = `${left}px`;
-  inspector.style.width = `${width}px`;
-  inspector.style.height = `${height}px`;
-
-  let label = "This element cannot be inspected.";
-  const sourceCode = findFiberByHostInstance(target)?._debugSource;
-  if (sourceCode) {
-    const { fileName, lineNumber, columnNumber } = sourceCode;
-    const file = fileName.split("/").at(-2) + "/" + fileName.split("/").at(-1);
-    label = `${file}:${lineNumber}:${columnNumber}`;
-  }
-
-  inspector.innerHTML = `
-  <span style="position: absolute;
-    padding: 3px;
-    color:white;
-    background-color: rgb(0 0 0 / 75%);
-    border-radius: 0 0 5px 5px;
-    white-space:nowrap;">
-    ${label}
-  </span>`;
+  if (overlay) overlay.inspect([target]);
 };
 
 const handleInspectorClick = (e: MouseEvent) => {
@@ -94,22 +49,11 @@ const handleInspectorClick = (e: MouseEvent) => {
   window.open(getVsCodeLink(fiber._debugSource));
 };
 
-const inspector = document.createElement("div");
-inspector.style.cssText = `
-  position: absolute;
-  z-index: 99999;
-  background-color: rgb(59 130 246 / 50%);
-  cursor: pointer;
-  pointer-events: none;
-  padding: 0;
-  display: none;`;
-
-document.body.appendChild(inspector);
-
 window.addEventListener("message", ({ data }: { data: string }) => {
   if (data === "inspect") {
-    if (!checkDevtoolsGlobalHookRenderer()) {
-      alert("This page is not available to use the React Inspector.");
+    if (!checkDevtoolsGlobalHook()) {
+      alert(`This page is not available to use the React Inspector.
+Make sure React Developer Tools is installed and DevTools is open.`);
       return;
     }
 
---
+++ src/utils.ts
@@ -0,0 +1,52 @@
+declare global {
+  interface Window {
+    __REACT_DEVTOOLS_GLOBAL_HOOK__: any;
+    __REACT_DEVTOOLS_TARGET_WINDOW__: any;
+  }
+}
+
+interface DebugSource {
+  columnNumber: number;
+  fileName: string;
+  lineNumber: number;
+}
+
+// TODO Refactoring needed ref react/packages/react-devtools-shared/src/backend/agent.js getBestMatchingRendererInterface
+export const checkDevtoolsGlobalHook = (): boolean =>
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__ &&
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers &&
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers.size > 0 &&
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers.get(1) &&
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.rendererInterfaces &&
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.rendererInterfaces.size > 0 &&
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.rendererInterfaces.get(1);
+
+// TODO Refactoring needed ref react/packages/react-devtools-shared/src/backend/agent.js getBestMatchingRendererInterface
+export const getDevtoolsGlobalHookRenderer = () => {
+  if (!checkDevtoolsGlobalHook()) return null;
+  return window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers.get(1);
+};
+
+// TODO Refactoring needed ref react/packages/react-devtools-shared/src/backend/agent.js getBestMatchingRendererInterface
+export const getDevtoolsGlobalHookRendererInterface = () => {
+  if (!checkDevtoolsGlobalHook()) return null;
+  return window.__REACT_DEVTOOLS_GLOBAL_HOOK__.rendererInterfaces.get(1);
+};
+
+export const getVsCodeLink = (sourceCode: DebugSource) =>
+  `vscode://file${sourceCode.fileName}:${sourceCode.lineNumber}:${sourceCode.columnNumber}`;
+
+export const findFiberByHostInstance = (
+  target: HTMLElement
+): { _debugSource: DebugSource } | null => {
+  if (!checkDevtoolsGlobalHook()) return null;
+
+  const renderer = getDevtoolsGlobalHookRenderer();
+  if (!renderer) return null;
+
+  const fiber = renderer.findFiberByHostInstance(target) || null;
+
+  return fiber && fiber._debugSource ? fiber : null;
+};
+
+export {};
