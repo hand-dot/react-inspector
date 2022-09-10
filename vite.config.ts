776b5b1beecc19de9c5260bc04ca12005b662021
Kyohei Fukuda <kyouhei.fukuda0729@gmail.com>
2022-09-10 12:26:58 +0900

    Version up 1.2.0

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ const manifest = defineManifest({
     default_title:
       "React Inspector: Press Ctrl+Shift+X (Command+Shift+X on Mac)",
   },
-  version: "1.1.0",
+  version: "1.2.0",
   content_scripts: [{ js: ["src/content.ts"], matches: ["<all_urls>"] }],
   devtools_page: "src/devtools.html",
   options_page: "src/options.html",
